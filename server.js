30bc5a779c081922f910584a56d15bbf58e2d05f
HarshaVardhannaidu07 <96299815+HarshaVardhannaidu07@users.noreply.github.com>
2025-05-15 04:12:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'backend.railway.internal', // Your frontend URL
+  origin: 'form-production-7787.up.railway.app', // Your frontend URL
   credentials: true // Enable cookies/auth if needed
 }));
 
