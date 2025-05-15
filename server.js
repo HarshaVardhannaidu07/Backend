38354aef7990bbe23549e5c33682c4ad96ea120e
HarshaVardhannaidu07 <96299815+HarshaVardhannaidu07@users.noreply.github.com>
2025-05-15 07:19:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const applicationRoutes = require('./routes/applications');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'form.railway.internal',
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Database connection
